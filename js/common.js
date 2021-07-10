473c2259bdf563f45492f1a4e28923f4b418d6ad
ryoya0110 <ryougura@icloud.com>
2021-07-10 16:10:53 +0900

    JQで動き追加

--- js/common.js
+++ js/common.js
@@ -30,3 +30,51 @@ $('.slick').slick({
 	pauseOnHover: false,
 	pauseOnDotsHover: false
 });
+
+const loadAnime = $('#load_animation');
+$(window).on('load', function () {
+	loadAnime.delay(2000).fadeOut(1000);
+});
+function stopload() {
+	loadAnime.delay(1000).fadeOut(700);
+}
+setTimeout('stopload()', 10000);
+
+function fadeIn(){
+  $('.fadeUpTrigger').each(function(){
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight){
+      $(this).addClass('fadeUp');
+    }else{
+      $(this).removeClass('fadeUp');
+    }
+  });
+
+  $('.fadeLeftTrigger').each(function(){
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight){
+      $(this).addClass('fadeLeft');
+    }else{
+      $(this).removeClass('fadeLeft');
+    }
+  });
+
+  $('.fadeRightTrigger').each(function(){
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight){
+      $(this).addClass('fadeRight');
+    }else{
+      $(this).removeClass('fadeRight');
+    }
+  });
+}
+
+$(window).scroll(function () {
+  fadeIn();
+});
